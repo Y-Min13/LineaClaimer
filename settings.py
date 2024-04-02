e3c3b07b1edcaf03c6e29b9a7e78d97892e8f134
Gabbe-x <72690663+Gabbe-x@users.noreply.github.com>
2024-04-02 17:07:22 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,3 +9,5 @@ ads_url = 'http://local.adspower.net:50325'
 thread_count = 1  # Количество потоков
 
 close_profile_enable = 0  # Закрывать ли профиль после квестов | 0 - Нет | 1 - Да |
+
+switch_mm = 0  # Включать ли импорт и логин метамаска | 0 - Нет | 1 - Да |
